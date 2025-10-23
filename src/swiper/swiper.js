f99d5df49b20e118b57b3b68452364dbde6b525d
Екатерина Тихонова <yekaterina.tikhonova.2007@bk.ru>
2025-10-23 14:16:11 +0900

    added all the swipers on the main page

---
+++ src/swiper/swiper.js
@@ -0,0 +1,20 @@
+import Swiper from "swiper";
+import Pagination from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+    modules: [Pagination],
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
